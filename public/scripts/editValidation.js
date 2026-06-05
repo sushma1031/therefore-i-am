0777b8f1fc3dd5f294beea78b354add68107317b
Sushma Jayaram <sushmajram@gmail.com>
2026-06-05 15:37:08 +0530

    feat: support removing post images on edit

--- public/scripts/editValidation.js
+++ public/scripts/editValidation.js
@@ -3,13 +3,19 @@ import isContentEmpty from "./contentValidation.js";
 const editForm = document.querySelector(".edit-form");
 const imageInput = document.querySelector(".edit-form #image");
 const imageSrc = document.querySelector(".edit-form #imageSource");
+const deleteImageInput = document.querySelector("#deleteImage");
+const removeImageBtn = document.querySelector("#removeImage");
+const thumbnail = document.querySelector(".thumbnail-img");
 
 let prevSrc = imageSrc.value;
+
+const originalThumbnailSrc = thumbnail.getAttribute("src");
+const hadOriginalImage = !thumbnail.classList.contains("d-none");
 
 editForm.addEventListener(
   "submit",
   (event) => {
-    if (imageInput.files.length === 0) {
+    if (imageInput.files.length === 0 && deleteImageInput.value !== "true") {
       imageSrc.value = prevSrc;
     }
     if (isContentEmpty() || !editForm.checkValidity()) {
@@ -22,9 +28,26 @@ editForm.addEventListener(
 );
 
 imageInput.addEventListener("change", (event) => {
+  const files = event.target.files;
+
+  if (files.length === 0) {
+    if (hadOriginalImage) {
+      thumbnail.src = originalThumbnailSrc;
+      thumbnail.classList.remove("d-none");
+      removeImageBtn.classList.remove("d-none");
+    } else {
+      thumbnail.classList.add("d-none");
+      removeImageBtn.classList.add("d-none");
+    }
+    deleteImageInput.value = "false";
+    imageSrc.value = prevSrc;
+    return;
+  }
+
+  deleteImageInput.value = "false";
   imageSrc.value = "";
-  let imgFile = event.target.files[0];
-  let thumbnail = document.querySelector(".thumbnail-img");
+
+  const imgFile = files[0];
   thumbnail.file = imgFile;
 
   const reader = new FileReader();
@@ -34,4 +57,14 @@ imageInput.addEventListener("change", (event) => {
   reader.readAsDataURL(imgFile);
 
   thumbnail.classList.remove("d-none");
+  removeImageBtn.classList.remove("d-none");
+});
+
+removeImageBtn.addEventListener("click", () => {
+  imageInput.value = "";
+  imageSrc.value = "";
+  deleteImageInput.value = "true";
+  thumbnail.src = "";
+  thumbnail.classList.add("d-none");
+  removeImageBtn.classList.add("d-none");
 });
